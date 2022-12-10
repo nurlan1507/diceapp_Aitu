3c589d4119c9d2bd0ec66548ecfbab8eab1a6093
Nurlan Baitassov <nur_nur2017@inbox.ru>
2022-12-10 15:18:38 +0600

    finish

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.diceapp
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 
 class Dice(val numSides:Int){
@@ -28,9 +31,19 @@ class MainActivity : AppCompatActivity() {
         val btn:Button = findViewById<Button>(R.id.button)
         btn.setOnClickListener {
             val num = dice.roll()
-            findViewById<TextView>(R.id.textView).text = num.toString()
-            val toast = Toast.makeText(this,"Rolled!",Toast.LENGTH_SHORT)
-            toast.show()
+            val text:TextView = findViewById<TextView>(R.id.textView)
+            text.text = num.toString()
+            val image:ImageView = findViewById<ImageView>(R.id.imageView)
+            when (text.text){
+                "1" -> image.setImageResource(R.drawable.dice_1)
+                "2" -> image.setImageResource(R.drawable.dice_2)
+                "3" -> image.setImageResource(R.drawable.dice_3)
+                "4" -> image.setImageResource(R.drawable.dice_4)
+                "5" -> image.setImageResource(R.drawable.dice_5)
+                "6" -> image.setImageResource(R.drawable.dice_6)
+            }
         }
     }
+
+
 }
